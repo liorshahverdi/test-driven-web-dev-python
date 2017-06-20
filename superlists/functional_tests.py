e91dda1c8cfb539cca863312e05d81c6aeef0802
Lior Shahverdi <liorshahverdi@gmail.com>
2017-06-20 18:42:07 -0400

    debugging StaleElementReferenceException

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -33,6 +33,7 @@ class NewVisitorTest(unittest.TestCase):
         # She types "Buy peacock feathers" into a text box (Edith's hobby
         # is tying fly-fishing lures)
         inputbox.send_keys('Buy peacock feathers')
+        self.browser.implicitly_wait(10)
 
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
